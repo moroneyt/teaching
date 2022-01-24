a9b600e8de32827a11c87455e958870c4a756fa8
Tim Moroney <t.moroney@qut.edu.au>
2022-01-25 09:41:18 +1000

    Update findmeplase.js

--- findmeplase.js
+++ findmeplase.js
@@ -1,1 +1,4 @@
-import hljs from "../imports/highlightjs.js"
+import hljs from 'https://unpkg.com/@highlightjs/cdn-assets@11.4.0/es/highlight.min.js';
+//  and it's easy to individually load & register additional languages
+import go from 'https://unpkg.com/@highlightjs/cdn-assets@11.4.0/es/languages/go.min.js';
+hljs.registerLanguage('go', go);
