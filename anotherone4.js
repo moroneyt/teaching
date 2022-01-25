cacc9287dcff8ba092951cecc210dce4c5bf62a7
Tim Moroney <t.moroney@qut.edu.au>
2022-01-25 14:25:46 +1000

    Delete anotherone4.js

--- anotherone4.js
+++
@@ -1,4 +0,0 @@
-import hljs from 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.3.1/build/es/highlight.min.js';
-//  and it's easy to individually load & register additional languages
-import matlab from 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.3.1/build/es/languages/matlab.min.js';
-hljs.registerLanguage('matlab', matlab);
